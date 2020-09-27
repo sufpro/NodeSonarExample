ece6f880f49aeb2fa673769fa0ef2943645fec28
CentOS <centos@centos7.linuxvmimages.local>
2020-09-27 18:33:56 -0400

    aa

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -3,16 +3,5 @@ libraries {
  merge=true
  xx='yy'
 }
- sonarqube {
-  merge =true
-cli_parameters = [
-           "-Dsonar.projectKey=projNodeJs",
-           "-Dsonar.sources=src",
-           "-Dsonar.tests=src",
-           "-Dsonar.inclusions=**",
-           "-Dsonar.test.inclusions='src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx'",
-           "-Dsonar.javascript.lcov.reportPaths='coverage/lcov.info'",
-           "-Dsonar.testExecutionReportPaths='coverage/test-reporter.xml'"
-        ] 
-}
+ sonarqube
 } 
