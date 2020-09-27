beee71d00c6999a4160e403f0a63808306c85cb4
CentOS <centos@centos7.linuxvmimages.local>
2020-09-27 04:48:34 -0400

    aa

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,5 +2,11 @@ libraries {
  maven
  sonarqube {
   projectKey='projNodeJs'
+  sources='src'
+  tests='src' 
+  inclusions='**'
+  testInclusions='src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx'
+  lcov='coverage/lcov.info'
+  testExecutionReportPaths='coverage/test-reporter.xml'
  }
 } 
