4a716822065e55e51cd55727aaa605398b901090
CentOS <centos@centos7.linuxvmimages.local>
2020-09-29 09:07:54 -0400

    aa

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner =  require('sonarqube-scanner');
 sonarqubeScanner(
     {
-        serverUrl:  'http://localhost:9000',
+        //serverUrl:  'http://localhost:9000',
         options : {
             'sonar.sources':  'src',
             'sonar.tests':  'src',
